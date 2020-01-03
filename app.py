f7eff7f7dd679d6613b54c9fe52eaaa8b764194b
Legaeldan <legaeldan@gmail.com>
2020-01-03 12:06:24 +0000

    Added route for viewing individual drink.

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+
 app.config["MONGO_DBNAME"] = 'drinks_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://testuser:testuserpassw0rd@myfirstcluster-ludvv.mongodb.net/test?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://testuser:testuserpassw0rd@myfirstcluster-ludvv.mongodb.net/drinks_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -25,8 +26,12 @@ def ingredients():
 
 @app.route('/collection')
 def collection():
-    return render_template('collection.html', 
-                            drinks=mongo.db.drinks.find())
+    return render_template('collection.html', drinks=mongo.db.drinks.find())
+
+@app.route('/view_drink/<drink_id>')
+def view_drink(drink_id):
+    the_drink =  mongo.db.drinks.find_one({"_id": ObjectId(drink_id)})
+    return render_template('viewdrink.html', drink=the_drink)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
