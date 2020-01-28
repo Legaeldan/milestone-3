cec5584c3e223eac868b7808979d8237e4a805e2
Legaeldan <legaeldan@gmail.com>
2020-01-28 22:58:57 +0000

    Combined classes for menu switch.

--- static/js/drinkSplash.js
+++ static/js/drinkSplash.js
@@ -1,19 +1,6 @@
 var toggledNavbar
 const isToggled = window.localStorage.getItem('toggled');
 
-if (isToggled == null){
-  console.log('null');
-  window.localStorage.setItem('toggled', 'disabled');
-  toggledNavbar = 'disabled';
-  console.log('state set to disabled');
-} else if (isToggled == 'disabled'){
-  toggledNavbar = 'disabled';
-  console.log('navbar disabled');
-} else if (isToggled == 'enabled'){
-  toggledNavbar = 'enabled';
-  console.log('Navbar is enabled');
-} ;
-
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('select').formSelect();
@@ -24,7 +11,7 @@ $(document).ready(function(){
     if ($('.remove-valign').length > 0){
       $('.container').removeClass("valign-wrapper")
     };
-    if (toggledNavbar == 'enabled'){
+    if (isToggled == 'enabled'){
       $('#nav-mobile').addClass("hide");
       $('.sidenav').addClass("hide");
       $('.sidenav-trigger').addClass("hide");
@@ -33,25 +20,19 @@ $(document).ready(function(){
 });
 
 $('#MainNavToggle').on("click", function(){
-  $('#nav-mobile').addClass("hide");
-  $('.sidenav').addClass("hide");
-  $('.sidenav-trigger').addClass("hide");
+  $('.toggleNavbar').addClass("hide");
   $('.fixed-action-btn').removeClass("hide");
   window.localStorage.setItem('toggled', 'enabled')
 });
 
 $('#FabNavToggle').on("click", function(){
   $('.fixed-action-btn').addClass("hide");
-  $('#nav-mobile').removeClass("hide");
-  $('.sidenav').removeClass("hide");
-  $('.sidenav-trigger').removeClass("hide");
+  $('.toggleNavbar').removeClass("hide");
   window.localStorage.setItem('toggled', 'disabled');
 });
 
 $('#sideNavToggle').on("click", function(){
-  $('.sidenav').addClass("hide");
-  $('.sidenav-trigger').addClass("hide");
-  $('#nav-mobile').addClass("hide");
+  $('.toggleNavbar').addClass("hide");
   $('.fixed-action-btn').removeClass("hide");
   window.localStorage.setItem('toggled', 'enabled');
 });
