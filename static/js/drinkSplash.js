6870fdab61801f5b99a380baa26b609fec99261e
Legaeldan <legaeldan@gmail.com>
2020-01-28 18:13:49 +0000

    Added functions for hiding navbar.

--- static/js/drinkSplash.js
+++ static/js/drinkSplash.js
@@ -17,11 +17,41 @@ if (isToggled == null){
 $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('select').formSelect();
+    $('.sidenav').sidenav();
     $('.modal').modal();
     $('.tooltipped').tooltip();
     $('.fixed-action-btn').floatingActionButton();
     if ($('.remove-valign').length > 0){
       $('.container').removeClass("valign-wrapper")
     };
+    if (toggledNavbar == 'enabled'){
+      $('#nav-mobile').addClass("hide");
+      $('.sidenav').addClass("hide");
+      $('.sidenav-trigger').addClass("hide");
+      $('.fixed-action-btn').removeClass("hide");
+    };
+});
 
-  });
+$('#MainNavToggle').on("click", function(){
+  $('#nav-mobile').addClass("hide");
+  $('.sidenav').addClass("hide");
+  $('.sidenav-trigger').addClass("hide");
+  $('.fixed-action-btn').removeClass("hide");
+  window.localStorage.setItem('toggled', 'enabled')
+});
+
+$('#FabNavToggle').on("click", function(){
+  $('.fixed-action-btn').addClass("hide");
+  $('#nav-mobile').removeClass("hide");
+  $('.sidenav').removeClass("hide");
+  $('.sidenav-trigger').removeClass("hide");
+  window.localStorage.setItem('toggled', 'disabled');
+});
+
+$('#sideNavToggle').on("click", function(){
+  $('.sidenav').addClass("hide");
+  $('.sidenav-trigger').addClass("hide");
+  $('#nav-mobile').addClass("hide");
+  $('.fixed-action-btn').removeClass("hide");
+  window.localStorage.setItem('toggled', 'enabled');
+});
